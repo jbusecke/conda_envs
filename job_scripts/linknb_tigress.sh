57c43711266390f763c90cfef6aa1daa871bcc7b
jbusecke <julius@ldeo.columbia.edu>
2017-09-13 17:15:59 -0400

    added chrome autoopen for tigress

--- job_scripts/linknb_tigress.sh
+++ job_scripts/linknb_tigress.sh
@@ -6,4 +6,4 @@ PORT=9997
 # Link a notebook running on tiger to current machine
 # assumes that 'tiger' is defined in the ssh config file
 ssh -N -f -L $NODENAME:$PORT:$NODENAME:$PORT tigress
-
+open --new -a /Applications/Google\ Chrome.app --args "http://localhost:$PORT"
