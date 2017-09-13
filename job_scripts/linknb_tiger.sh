a8b6aa49965d9260aeb802567fcdaa01cdeda67b
jbusecke <julius@ldeo.columbia.edu>
2017-09-13 14:26:24 -0400

    updates to linknb tiger

--- job_scripts/linknb_tiger.sh
+++ job_scripts/linknb_tiger.sh
@@ -6,3 +6,4 @@ PORT=9998
 # Link a notebook running on tiger to current machine
 # assumes that 'tiger' is defined in the ssh config file
 ssh -N -f -L $PORT:$NODENAME:$PORT tiger
+open --new -a /Applications/Google\ Chrome.app --args "http://localhost:$PORT"
