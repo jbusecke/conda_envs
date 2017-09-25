24e28ec218b7ed2f4395c5fe7d625d4de14fcc32
jbusecke <julius@ldeo.columbia.edu>
2017-09-25 14:09:03 -0400

    fixed link tiger script

--- job_scripts/linknb_tiger.sh
+++ job_scripts/linknb_tiger.sh
@@ -6,5 +6,6 @@ NODENAME=$1
 # assumes that 'tiger' is defined in the ssh config file
 open -na /Applications/Google\ Chrome.app --args "http://localhost:7772" "http://localhost:7771/status"
 # ssh -N -f -L 7771:$NODENAME:7771 -L 7772:$NODENAME:7772 tiger
-ssh -N -L 7771:$NODENAME:7771 -L 7772:$NODENAME:7772 tiger
+ssh -N -L 7771:$NODENAME-ib0:7771 -L 7772:$NODENAME:7772 tiger
+# The diagnostic interface has to be connected through via ib0 interface
 # open --new -a /Applications/Google\ Chrome.app --args "http://localhost:7772" "http://localhost:7771/status"
