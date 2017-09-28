6bdf47964bc44a5192440d68c93ea8d9d9b111c1
jbusecke <julius@ldeo.columbia.edu>
2017-09-28 11:11:10 -0400

    added modules and updated linkscript

--- job_scripts/linknb_tigress.sh
+++ job_scripts/linknb_tigress.sh
@@ -4,6 +4,5 @@ NODENAME=tigressdata.princeton.edu
 
 # Link a notebook running on tigressdata to current machine
 # assumes that 'tigress' is defined in the ssh config file
-
-ssh -N -f -L 9992:$NODENAME:9992 -L 9991:$NODENAME:9991 tigress
+ssh -N -f -L 9992:$NODENAME:9992 -L 9991:localhost:9991 tigress
 open --new -a /Applications/Google\ Chrome.app --args "http://localhost:9992" "http://localhost:9991"
